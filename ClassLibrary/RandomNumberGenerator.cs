e6da0d754c590b3a62ba1843e9be5e30046b79f7
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-02-27 10:44:44 +0200

    Added class StatisticsCalculator

--- ClassLibrary/RandomNumberGenerator.cs
+++ ClassLibrary/RandomNumberGenerator.cs
@@ -1,7 +1,30 @@
 ﻿namespace ClassLibrary
 {
-    public class Class1
+    public class RandomNumberGenerator
     {
+        private int a; // Множник
+        private int c; // Приріст
+        private int m; // Модуль (2^31 - 1)
+        private int seed; // Початкове значення
 
+        public RandomNumberGenerator(int a, int c, int m, int seed)
+        {
+            this.a = a;
+            this.c = c;
+            this.m = m;
+            this.seed = seed;
+        }
+
+        public int[] GenerateRandomSequence(int K)
+        {
+            int[] randomValues = new int[K];
+            int x = seed;
+            for (int i = 0; i < K; i++)
+            {
+                x = (a * x + c) % m;
+                randomValues[i] = x;
+            }
+            return randomValues;
+        }
     }
 }
---
+++ ClassLibrary/StatisticsCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class StatisticsCalculator
+    {
+        public double CalculateMean(int[] values)
+        {
+            int sum = 0;
+            foreach (int value in values)
+            {
+                sum += value;
+            }
+            return (double)sum / values.Length;
+        }
+
+        public double CalculateVariance(int[] values, double mean)
+        {
+            double sumOfSquares = 0;
+            foreach (int value in values)
+            {
+                double deviation = value - mean;
+                sumOfSquares += deviation * deviation;
+            }
+            return sumOfSquares / (values.Length - 1);
+        }
+    }
+}
